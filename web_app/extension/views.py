37c77bb84068df6f498cca62649b33fc1e6cfb51
NathanDeMaria <naterstd@gmail.com>
2015-07-25 18:42:32 -0500

    Edit add view to save Article

--- web_app/extension/views.py
+++ web_app/extension/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
+from extension.models import Article
+from datetime import datetime
 
 
 @require_http_methods(['POST'])
 @csrf_exempt
 def add(request):
+
     if 'good.x' in request.POST:
-        print "Good"
+        response = 'L'
     elif 'bad.x' in request.POST:
-        print "Bad"
+        response = 'D'
     else:
         raise ValueError("Unrecognized response")
+
+    a = Article(url=request.POST['url'], access_time=datetime.now(), response=response)
+    a.save()
 
     return render(request, "rated.html")

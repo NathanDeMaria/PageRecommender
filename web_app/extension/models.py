b6ec7025547ec658a2dd694394bb3384100b3a78
NathanDeMaria <naterstd@gmail.com>
2015-07-25 17:52:44 -0500

    Add Article model

--- web_app/extension/models.py
+++ web_app/extension/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    RESPONSES = (
+        ('L', 'like'),
+        ('D', 'dislike')
+    )
+    url = models.CharField(max_length=100)
+    access_time = models.DateField()
+    response = models.CharField(max_length=1, choices=RESPONSES)

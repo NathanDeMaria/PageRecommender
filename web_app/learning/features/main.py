4ece7ca22f617d120640d8f53efbf7af8a817ec4
NathanDeMaria <naterstd@gmail.com>
2015-07-26 15:22:52 -0500

    Add skeleton for main feature generation

---
+++ web_app/learning/features/main.py
@@ -0,0 +1,38 @@
+from itertools import imap
+from text import add_text
+
+
+def create_data(feature_functions):
+    """
+    Creates the data in the form of a list of dicts,
+    where each dict represents an Article and its features
+    :param feature_functions: list of functions to apply to rows of the data
+    :return: list of article dicts, with features included
+    """
+    articles = Article.objects.all()
+
+    for feature in feature_functions:
+        articles = imap(feature, articles)
+
+    return articles
+
+
+def to_dict(article):
+    """
+    Turns an Article into a dict representing its base info
+    :param article: Article model
+    :return: dict representing the Article model
+    """
+    return dict(
+        time=article.access_time,
+        response=article.response,
+        url=article.url
+    )
+
+
+if __name__ == '__main__':
+    import os
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web_app.settings")
+    from extension.models import Article
+    feature_generators = [to_dict, add_text]
+    print list(create_data(feature_generators))

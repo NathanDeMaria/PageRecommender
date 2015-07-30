3ecf80808a4d5825d3d7db08bc15f6c0897b675d
Keenan Hawekotte <keenan.hawekotte@hudl.com>
2015-07-30 08:02:47 -0500

    Keenan's turn to be bad.
    FIxed some bad logic on when to show, and when ot to show loading gif.

--- chrome_extension/recommender.js
+++ chrome_extension/recommender.js
@@ -2,9 +2,10 @@ $(document).ready(function() {
     chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
         var tab_url = tabs[0].url;
         $("#url-input").val(tab_url);
-        $("#loading-div").hide()
+        $("#loading-div").show();
         $.post("http://127.0.0.1:8000/extension/load", {url: tab_url},function(data) {
             $("#percent-val").text(data);
+            $("#loading-div").hide();
         });
     });
 });

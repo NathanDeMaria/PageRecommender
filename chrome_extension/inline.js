006abf4fd084d6c878ee1451dd40510dc666d4b1
Keenan Hawekotte <keenan.hawekotte@hudl.com>
2015-07-30 13:17:27 -0500

    Nathan, I am competing with you for the worst.
    Updated to work for all main pages of datatau. Also, added check for simple linking weirdness in datatau (no http in link).

--- chrome_extension/inline.js
+++ chrome_extension/inline.js
@@ -11,7 +11,9 @@ $(document).ready(function() {
             $("a").each(function(ind, val) {
                 var tmp_parent = $(this).parent().get(0);
 
-                if (tmp_parent.tagName == "TD" && tmp_parent.className == 'title' && $(this).text() != 'More') {
+                if (tmp_parent.tagName == "TD" && tmp_parent.className == 'title' && $(this).text() != 'More' &&
+                    $(this).attr('href').indexOf("datatau.com") == -1 &&
+                    $(this).attr('href').indexOf("http") != -1) {
                     //console.log(tmp_parent.tagName + ' ::: ' + tmp_parent.className);
                     var tmp_link = $(this);
                     var tmp_url = tmp_link.attr('href');
@@ -21,6 +23,7 @@ $(document).ready(function() {
                             //$("#percent-val").text(data);
                             //$("#loading-div").hide();
                         }).fail(function() {
+                            console.log(tmp_url);
                             console.log('No connection can be made to the server. Please check your internet connection' +
                                         ' or warn the admin that the server is down.')
                         });
